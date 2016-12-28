b5c50a3e53db0df9d8049f300ac86a8d7d99220b
soupman99 <youn2493@gmail.com>
2016-12-28 16:31:10 -0600

    added a todo

--- webpack_config/webpack.dev.js
+++ webpack_config/webpack.dev.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 
-//TODO: Fix this so that we can use 1 set of configureable files for electron instead of 2 static versions (which is stupid)
+//TODO:Fix this so that we can use 1 set of configureable files for electron instead of 2 static versions (which is stupid)
 
 console.log(process.env.NODE_ENV);
 module.exports = {
